b813f73bc077555304ce286f4615776b7a0962cc
falmaskari <130557093+falmaskari@users.noreply.github.com>
2023-05-16 00:17:09 +0400

    Update Program.cs

--- Practice_D6/Program.cs
+++ Practice_D6/Program.cs
@@ -4,31 +4,13 @@
     {
         static void Main(string[] args)
         {
-            /*int[] Arr = new int[7] { 1, 2, 7, 5, 8, 2, 1 };
-            for (int i = 0; i < Arr.Length; i++)
-            {
-                for(int j = 0; j < Arr.Length; j++)
-                {
-                    if (Arr[i] == Arr[j])
-                    {
-                        Console.WriteLine(Arr[i]);
-                        break;
-                    }
-                }
-            }*/
-
-            /*Person p1 = new Person();
+            Person p1 = new Person();
             p1.Setfname("Fatma");
             p1.Setlname("Almaskari");
             p1.SetAge(23);
             p1.SetSalary(900.5);
             p1.SetRel("No");
-            Console.WriteLine(p1.printInfo());*/
-
-            Bank bank = new Bank();
-            bank.Setdeposite(500);
-            bank.Setwithdraw(500);
-            bank.withDraw(500);
+            Console.WriteLine(p1.printInfo());
         }
     }
 }
