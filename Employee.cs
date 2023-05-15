e2fbceb52fa270892bf49dc32a7b046ceed211a5
falmaskari <130557093+falmaskari@users.noreply.github.com>
2023-05-15 22:30:32 +0400

    Add files via upload

---
+++ Employee.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice4
+{
+    internal class Employee //Class declaration
+    {
+        public const double TAX = 0.03; //Constant value
+        public string fName;
+        public string lName;
+        public double wags;
+        public double loggedHours;
+
+        public int addone(ref int x)
+        {
+            return x + 1;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System.Xml.Serialization;
+
+namespace Practice4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee emp = new Employee(); //Create object from class
+
+            Console.WriteLine("Enter employee no:");
+            int nums = Convert.ToInt32(Console.ReadLine());
+
+            int[] arr = new int[nums];
+            for (int i = 1; i <= arr.Length; i++)
+            {
+                Console.WriteLine("Employee"+i);
+                Console.WriteLine("First name");
+                emp.fName = Console.ReadLine();
+                Console.WriteLine("Last name");
+                emp.lName = Console.ReadLine();
+                Console.WriteLine("Wags");
+                emp.wags = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Logged hours");
+                emp.loggedHours = Convert.ToInt32(Console.ReadLine());
+
+                double totalSalary = emp.loggedHours * emp.wags; //Calculate total salary
+                double taxRate = emp.loggedHours * emp.wags * Employee.TAX; //Calculate tax rate
+
+                double netSalary = totalSalary - taxRate; //Calculate net salary
+                Console.WriteLine("Salary:" + netSalary);
+                Console.WriteLine();
+
+                int y = 18;
+                emp.addone(ref y);
+                Console.WriteLine(y);
+            }
+        }
+    }
+}
