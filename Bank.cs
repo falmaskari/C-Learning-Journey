95767ff4146cc8a6bf3afc9f1c28c9f015ae28d8
falmaskari <130557093+falmaskari@users.noreply.github.com>
2023-05-16 00:07:42 +0400

    Add files via upload

---
+++ Bank.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Day_s_Problem
+{
+    internal class BankAcount
+    {
+        private const int MAX_AMOUNT = 1000;
+        private string accountNumber;
+        private decimal balance;
+        public decimal getBalance(decimal Balance)
+        {
+            return balance;
+        }
+        public string Account(double Amount)
+        {
+            this.amount = Amount;
+        }
+        public double Getdwithdraw(double Amount)
+        {
+            return amount;
+        }
+        public double withdraw(double amount)
+        {
+            if()
+        }
+
+    }
+}
