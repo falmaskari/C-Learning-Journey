60d3ddf28d76f04feca4861902a691824962f497
falmaskari <130557093+falmaskari@users.noreply.github.com>
2023-05-24 20:09:00 +0400

    Add files via upload

---
+++ Generics/Generics/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Generics
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ARR<int> arr = new ARR<int>();
+            arr.add(1);
+            arr.add(2);
+            arr.add(3);
+            arr.add(4);
+            arr.add(5);
+            arr.remove(0);
+           arr.display(); 
+
+        }
+    }
+}
