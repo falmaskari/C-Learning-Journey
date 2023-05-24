df652ddc2a4cb71bd0ad5e4c424f75cd8d61e376
falmaskari <130557093+falmaskari@users.noreply.github.com>
2023-05-24 20:10:47 +0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System.Runtime.InteropServices;
+using static Event.Weather;
+
+namespace Event
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Weather weather = new Weather();
+            weather.onWetherChanged += Weather_onWetherChanged;
+            weather.changeWeather(16);
+            weather.changeWeather(24);
+            weather.changeWeather(50);
+        }
+
+        private static void Weather_onWetherChanged(Weather weather)
+        {
+            if (weather.Temperture < 17)
+            {
+                Console.ForegroundColor = ConsoleColor.Blue;
+                Console.WriteLine($"Rainy and cold");
+            }
+            else if (weather.Temperture > 17 && weather.Temperture < 25)
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("Cloudy and no rain");  
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Hot and melt");
+            }
+            Console.ResetColor();
+        }
+    }
+}
---
+++ Weather.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Event
+{
+    class Weather
+    {
+        public delegate void WeatherChanged(Weather weather );
+        public event WeatherChanged onWetherChanged;
+
+        public int temperature;
+        public string condition;
+
+        public int Temperture { get => this.temperature; set => this.temperature = value; }
+
+        public string Condition => this.condition;
+
+        
+        
+        public void changeWeather(int temperature)
+        {
+            this.temperature = temperature;
+            if(onWetherChanged != null)
+            {
+                onWetherChanged(this);
+            }
+        } 
+    }
+}
