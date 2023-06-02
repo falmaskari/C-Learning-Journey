7deb374e368f4be6d19d83a1e213fe815b050678
Fatma Almaskari <130557093+falmaskari@users.noreply.github.com>
2023-06-02 16:28:07 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Enum
+{
+    internal class Program
+    {
+        /*enum Days
+        {
+            Saturday = 123456789,
+            Sunday,
+            Monday,
+            Tuesday,
+            Wednesday,
+            Thursday,
+            Friday,
+        }*/
+        static void Main(string[] args)
+        {
+            Patient patient = new Patient();
+            patient.Choose();
+            /*Days Days = Days.Tuesday;
+            Console.WriteLine(Days);
+
+            Days Days1 = Days.Tuesday;
+            Console.WriteLine((int)Days1);*/
+        }
+        
+            
+    }
+}
