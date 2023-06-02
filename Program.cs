2f1a0405a8ee41a716811f35acd96651ac535875
Fatma Almaskari <130557093+falmaskari@users.noreply.github.com>
2023-06-02 14:53:32 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿namespace Inheritance
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Cat cat = new Cat(); //Create an instance from class
+            cat.name = "LuluCati";
+            cat.age = 1;
+            cat.gender = "Female";
+            Console.WriteLine(cat);
+            cat.move();
+            cat.sound();
+            cat.Makesound();
+
+            Dog dog = new Dog();
+            dog.name = "Bobby";
+            dog.age = 3;
+            dog.category = "Domestic";
+            dog.move();
+            dog.sound();
+            dog.Makesound();
+
+            Eagl eagl = new Eagl();
+            eagl.name = "Apollo";
+            eagl.age = 10;
+            eagl.gender = "Male";
+            eagl.move();
+            eagl.fly();
+            dog.Makesound();
+
+            Honda honda = new Honda(1, "Honda", "First class", 1881);
+            honda.Move();
+            honda.Stop();
+
+            Cterpillar cterpillar = new Cterpillar(1, "Honda", "First class", 1881);
+            cterpillar.Move();
+            cterpillar.Stop();
+            cterpillar.Load();
+            cterpillar.Unload();
+        }
+    }
+}
---
+++ Vehicle.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance
+{
+    class Vehicle
+    {
+        public int id;
+        public string name;
+        public string description;
+
+        public Vehicle(int id, string name, string description) //Create constructor
+        {
+            this.id = id;
+            this.name = name;
+            this.description = description;
+        }
+    }
+    interface IDriveable //Create interface
+    {
+        void Move();
+        void Stop();
+    }
+    interface ILoader
+    {
+        void Load();
+        void Unload();
+    }
+    class Honda : Vehicle, IDriveable
+    {
+        public int yearOfManufacturer;
+        public Honda(int id, string name, string description, int yearOfManufacturer) : base(id, name, description)
+        {
+            this.yearOfManufacturer = yearOfManufacturer;
+        }
+        public void Move()
+        {
+            Console.WriteLine("I move");
+        }
+        public void Stop()
+        {
+            Console.WriteLine("I stop");
+        }
+    }
+    class Cterpillar : Vehicle, IDriveable, ILoader
+        {
+            public int yearOfManufacturer;
+            public Cterpillar(int id, string name, string description, int yearOfManufacturer) : base(id, name, description)
+            {
+                this.yearOfManufacturer = yearOfManufacturer;
+            }
+
+            public void Load()
+            {
+                Console.WriteLine("I load");
+            }
+
+            public void Move()
+            {
+                Console.WriteLine("I move");
+            }
+
+            public void Stop()
+            {
+                Console.WriteLine("I stop");
+            }
+
+            public void Unload()
+            {
+                Console.WriteLine("I unload");
+            }
+        }
+}
