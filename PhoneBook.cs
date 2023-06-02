5c25c12d2be904c5f01ce2984b0b253b72c8e872
Fatma Almaskari <130557093+falmaskari@users.noreply.github.com>
2023-06-02 16:08:48 +0300

    Add files via upload

---
+++ PhoneBook.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Indexer
+{
+    class PhoneBook
+    {
+        public string[] Names; //Declare properties
+        public string[] Numbers;
+        public int Size;
+        public PhoneBook(int _size) //Create constructor
+        {
+            this.Size = _size;
+            Names = new string[_size];
+            Numbers = new string[_size];
+        }
+
+        public string this[int index, string Name] //Create indexer that set index and name and get number 
+        {
+            //Setting values
+            set
+            {
+                if (index > 0 && index < Size)
+                {
+                    this.Names[index] = Name;
+                    this.Numbers[index] = value;
+                }
+            }
+            //Getting value
+            get
+            {
+                for (int i = 0; i < Size; i++)
+                {
+                    if (Names[i] == Name)
+                    {
+                        return Numbers[i];
+                    }
+                }
+                return null;
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Indexer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            PhoneBook phone = new PhoneBook(2); //Create instance from class
+            phone[0, "Fatma"] = "1234";
+            phone[1, "Saleh"] = "5678";
+            Console.WriteLine(phone[0, "Fatma"]);
+            Console.WriteLine(phone[1, "Saleh"]);
+        }
+    }
+}
