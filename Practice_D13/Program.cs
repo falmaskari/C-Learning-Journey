cf9852e304b49b5782790ab6f4d0f8ae5e87fc14
Fatma Almaskari <130557093+falmaskari@users.noreply.github.com>
2023-06-03 21:18:11 +0300

    Add files via upload

--- Practice_D13/Program.cs
+++ Practice_D13/Program.cs
@@ -1,37 +1,19 @@
-﻿using System.Runtime.InteropServices;
-using static Event.Weather;
+﻿using System;
 
-namespace Event
+namespace Generics
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Weather weather = new Weather();
-            weather.onWetherChanged += Weather_onWetherChanged;
-            weather.changeWeather(16);
-            weather.changeWeather(24);
-            weather.changeWeather(50);
-        }
-
-        private static void Weather_onWetherChanged(Weather weather)
-        {
-            if (weather.Temperture < 17)
-            {
-                Console.ForegroundColor = ConsoleColor.Blue;
-                Console.WriteLine($"Rainy and cold");
-            }
-            else if (weather.Temperture > 17 && weather.Temperture < 25)
-            {
-                Console.ForegroundColor = ConsoleColor.Green;
-                Console.WriteLine("Cloudy and no rain");  
-            }
-            else
-            {
-                Console.ForegroundColor = ConsoleColor.Red;
-                Console.WriteLine("Hot and melt");
-            }
-            Console.ResetColor();
+            ARR<int> arr = new ARR<int>();
+            arr.add(1);
+            arr.add(2);
+            arr.add(3);
+            arr.add(4);
+            arr.add(5);
+            arr.remove(0);
+            arr.display(); 
         }
     }
 }
